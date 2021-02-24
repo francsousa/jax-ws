3a51d9b5fe319dab933b3278cc4f7b4cd6554de3
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-02-24 07:45:29 -0300

    Trabalhando com cabeçalhos

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,25 +3,51 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
 	
-	@WebMethod(operationName = "todosOsItens")
-	@ResponseWrapper(localName = "itens")
-//	@RequestWrapper(localName = "listaItens")
+	@WebMethod(operationName = "TodosOsItens")
 	@WebResult(name = "item")
-	public List<Item> getItens() {
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
 		
 		System.out.println("Chamando getItens()");
-	    return dao.todosItens();
+		
+	    List<Filtro> listaFiltros = filtros.getLista();
+	    List<Item> lista = dao.todosItens(listaFiltros);
+	    
+	    return new ListaItens(lista);
+	}
+	
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token, 
+			@WebParam(name = "item") Item item) throws AutorizacaoException {
+		
+		System.out.println("cadastrando item " + item + ", Token: " + token);
+		
+		boolean valido = new TokenDao().ehValido(token);
+		
+		if (!valido) {
+			throw new AutorizacaoException("Autorização falhou");
+		}
+		
+		this.dao.cadastrar(item);
+		
+		return item;
 	}
 }
