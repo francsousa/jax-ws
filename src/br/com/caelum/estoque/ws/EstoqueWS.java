7454ab433385780cd26cfef16adfbdcbbc4113a7
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-02-26 09:26:33 -0300

    Entendendo os estilos Document e RPC

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -18,11 +22,12 @@ import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
+@SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
 	
-	@WebMethod(operationName = "TodosOsItens")
+	@WebMethod(action = "CadastrarItem", operationName = "TodosOsItens")
 	@WebResult(name = "item")
 	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
 		
--- src/br/com/caelum/estoque/ws/PublicaWebService.java
+++ src/br/com/caelum/estoque/ws/PublicaWebService.java
@@ -5,16 +5,19 @@ import javax.xml.ws.Endpoint;
 import br.com.caelum.estoque.services.RelatorioService;
 
 public class PublicaWebService {
-	
+
 	public static void main(String[] args) {
-		
-		// EstoqueWS service = new EstoqueWS();
-		RelatorioService service = new RelatorioService();
-		// String url = "http://localhost:8080/estoquews";
-		String url = "http://localhost:8080/relatorioservice";
-		
-		System.out.println("Service rodando " + url + "?wsdl");
-		
-		Endpoint.publish(url, service);
+
+		EstoqueWS estoqueService = new EstoqueWS();
+		RelatorioService relatorioService = new RelatorioService();
+
+		String urlEstoque = "http://localhost:8080/estoquews";
+		String urlRelatorio = "http://localhost:8080/relatorioservice";
+
+		System.out.println("Service rodando " + urlEstoque + "?wsdl");
+		System.out.println("Service rodando " + urlRelatorio + "?wsdl");
+
+		Endpoint.publish(urlEstoque, estoqueService);
+		Endpoint.publish(urlRelatorio, relatorioService);
 	}
 }
